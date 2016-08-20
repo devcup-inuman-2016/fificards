97cf10e7eca2079632573016cf6447926620637a
Richard Neil Ilagan <me@richardneililagan.com>
2016-08-20 18:35:34 +0800

    Adds jQuery, Lodash, onmount, bootstrap to JS export.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,6 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+// :: 3rd party libraries
+window.jQuery = window.$ = require('jquery')
+window.onmount = require('onmount')
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
+window.lodash = window._ = require('lodash/core')
+require('bootstrap-sass')
